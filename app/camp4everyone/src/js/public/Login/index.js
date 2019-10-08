81ffa2a397fe8d040b3d13f8be5c0bd2820315a4
Isaac Palacio <eipalacio@uniorte.edu.co>
2019-10-08 15:25:00 -0500

    Recovery and CategoryNav. No more clowns.

--- app/camp4everyone/src/js/public/Login/index.js
+++ app/camp4everyone/src/js/public/Login/index.js
@@ -1,72 +1,72 @@
-import React from 'react'
+import React from "react";
 
-import { withStyles, makeStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import Container from '@material-ui/core/Container'
-import MaterialIcon from 'material-icons-react'
-import { Link } from 'react-router-dom'
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import MaterialIcon from "material-icons-react";
+import { Link } from "react-router-dom";
 
 const CssTextField = withStyles({
   root: {
-    '& label.Mui-focused': {
-      color: '#3a9679'
+    "& label.Mui-focused": {
+      color: "#3a9679"
     },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: '#3a9679'
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#3a9679"
     },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: 'white'
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "white"
       },
-      '&:hover fieldset': {
-        borderColor: '#3a9679'
+      "&:hover fieldset": {
+        borderColor: "#3a9679"
       },
-      '&.Mui-focused fieldset': {
-        borderColor: '#3a9679'
+      "&.Mui-focused fieldset": {
+        borderColor: "#3a9679"
       }
     }
   }
-})(TextField)
+})(TextField);
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap'
+    display: "flex",
+    flexWrap: "wrap"
   },
   margin: {
     margin: theme.spacing(1)
   }
-}))
+}));
 
 export default function Login() {
-  const classes = useStyles()
+  const classes = useStyles();
   const [values, setValues] = React.useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: ""
+  });
 
   const handleChange = prop => event => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
   return (
-    <Container component='div' className='login-container'>
+    <Container component="div" className="login-container">
       <Container
-        component='main'
-        maxWidth='xs'
-        className='login'
-        color='primary'
+        component="main"
+        maxWidth="xs"
+        className="login"
+        color="primary"
       >
         <form className={classes.root} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} className='login-icon'>
-              <MaterialIcon icon='account_circle' color='#ffffff' size={80} />
+            <Grid item xs={12} sm={12} className="login-icon">
+              <MaterialIcon icon="account_circle" color="#ffffff" size={80} />
             </Grid>
             <Grid item xs={12} sm={12}>
-              <Typography component='h1' variant='h5'>
+              <Typography component="h1" variant="h5">
                 Log in to Camp4Everyone
               </Typography>
             </Grid>
@@ -74,52 +74,52 @@ export default function Login() {
               className={classes.margin}
               required
               fullWidth
-              label='Email'
-              variant='outlined'
-              id='email'
-              type='email'
-              name='email'
-              autoComplete='email'
+              label="Email"
+              variant="outlined"
+              id="email"
+              type="email"
+              name="email"
+              autoComplete="email"
               value={values.email}
-              onChange={handleChange('email')}
+              onChange={handleChange("email")}
             />
             <CssTextField
               className={classes.margin}
               required
               fullWidth
-              label='Password'
-              variant='outlined'
-              id='password'
-              type='password'
-              name='password'
-              autoComplete='password'
+              label="Password"
+              variant="outlined"
+              id="password"
+              type="password"
+              name="password"
+              autoComplete="password"
               value={values.password}
-              onChange={handleChange('password')}
+              onChange={handleChange("password")}
             />
           </Grid>
           <Grid item xs={12}>
             <Button
-              type='submit'
+              type="submit"
               fullWidth
-              variant='contained'
-              color='secondary'
-              style={{ marginTop: '0.8rem' }}
+              variant="contained"
+              color="secondary"
+              style={{ marginTop: "0.8rem" }}
             >
               Log in
             </Button>
           </Grid>
-          <Grid item xs={12} spacing={2} style={{ marginTop: '0.8rem' }}>
-            <Typography component='p'>
-              Already using Camp4Everyone?{' · '}
-              <Link to='/passwordRecovery'>Forgot password?</Link>
+          <Grid item xs={12} spacing={2} style={{ marginTop: "0.8rem" }}>
+            <Typography component="p">
+              Already using Camp4Everyone?{" · "}
+              <Link to="/passwordRecovery">Forgot password?</Link>
             </Typography>
-            <Typography component='p'>
-              New to Camp4Everyone?{' · '}
-              <Link to='/signup'>Sign up now »</Link>
+            <Typography component="p">
+              New to Camp4Everyone?{" · "}
+              <Link to="/signup">Sign up now »</Link>
             </Typography>
           </Grid>
         </form>
       </Container>
     </Container>
-  )
+  );
 }
