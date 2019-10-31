8d9f2418631b80f17c29d8dfe0ce30ec6a712ca6
eipalacio <eipalacio@uninorte.edu.co>
2019-10-31 18:42:41 -0500

    Cambios de prueba Update Databse Info

--- app/camp4everyone/src/js/public/Login/index.js
+++ app/camp4everyone/src/js/public/Login/index.js
@@ -1,60 +1,60 @@
-import React from 'react';
+import React from "react";
 
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import MaterialIcon from 'material-icons-react';
-import { Link, Redirect } from 'react-router-dom';
-import Snackbar from '@material-ui/core/Snackbar';
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import MaterialIcon from "material-icons-react";
+import { Link, Redirect } from "react-router-dom";
+import Snackbar from "@material-ui/core/Snackbar";
 
-import { SnackbarContentWrapper } from './../../utils/SnackbarContentWrapper';
-import Loading from './../../utils/Loading';
-import { login } from './../../services/firebase';
+import { SnackbarContentWrapper } from "./../../utils/SnackbarContentWrapper";
+import Loading from "./../../utils/Loading";
+import { login, getCurrentUser, updateUser } from "./../../services/firebase";
 
 const CssTextField = withStyles({
   root: {
-    '& label.Mui-focused': {
-      color: '#3a9679',
+    "& label.Mui-focused": {
+      color: "#3a9679"
     },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: '#3a9679',
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#3a9679"
     },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: 'white',
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "white"
       },
-      '&:hover fieldset': {
-        borderColor: '#3a9679',
+      "&:hover fieldset": {
+        borderColor: "#3a9679"
       },
-      '&.Mui-focused fieldset': {
-        borderColor: '#3a9679',
-      },
-    },
-  },
+      "&.Mui-focused fieldset": {
+        borderColor: "#3a9679"
+      }
+    }
+  }
 })(TextField);
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   margin: {
-    margin: theme.spacing(1),
-  },
+    margin: theme.spacing(1)
+  }
 }));
 
 export default function Login(props) {
   const classes = useStyles();
   const [values, setValues] = React.useState({
-    email: '',
-    password: '',
+    email: "",
+    password: ""
   });
 
-  const [variant, setVariant] = React.useState('');
-  const [message, setMessage] = React.useState('');
+  const [variant, setVariant] = React.useState("");
+  const [message, setMessage] = React.useState("");
   const [open, setOpen] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [toAdmin, setToAdmin] = React.useState(false);
@@ -65,12 +65,13 @@ export default function Login(props) {
     if (values.email && values.password) {
       login(values.email, values.password)
         .then(user => {
-          setVariant('success');
-          setMessage('Usuario autorizado');
+          setVariant("success");
+          setMessage("Usuario autorizado");
+          updateUser(getCurrentUser().id, "cambiooo");
           setOpen(true);
 
           setTimeout(() => {
-            sessionStorage.setItem('user', user.user.uid);
+            sessionStorage.setItem("user", user.user.uid);
             setLoading(false);
             setToHome(true);
             props.setAuthentication(true);
@@ -78,18 +79,19 @@ export default function Login(props) {
         })
         .catch(err => {
           setTimeout(() => {
-            setVariant('error');
-            setMessage('Credenciales inválidas');
+            console.log(err);
+            setVariant("error");
+            setMessage("Credenciales inválidas");
             setOpen(true);
-            setValues.password = '';
+            setValues.password = "";
             setLoading(false);
           }, 2000);
         });
     } else {
-      setVariant('error');
-      setMessage('Digite todos los campos');
+      setVariant("error");
+      setMessage("Digite todos los campos");
       setOpen(true);
-      setValues.password = '';
+      setValues.password = "";
       setLoading(false);
     }
   };
@@ -103,22 +105,22 @@ export default function Login(props) {
   };
 
   return (
-    <Container component='div' className='login-container'>
+    <Container component="div" className="login-container">
       <Container
-        component='main'
-        maxWidth='xs'
-        className='login'
-        color='primary'
+        component="main"
+        maxWidth="xs"
+        className="login"
+        color="primary"
       >
         {loading && <Loading />}
-        {toHome ? <Redirect to='/home' /> : null}
+        {toHome ? <Redirect to="/home" /> : null}
         <form className={classes.root} onSubmit={handleSubmit} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} className='login-icon'>
-              <MaterialIcon icon='account_circle' color='#ffffff' size={80} />
+            <Grid item xs={12} sm={12} className="login-icon">
+              <MaterialIcon icon="account_circle" color="#ffffff" size={80} />
             </Grid>
             <Grid item xs={12} sm={12}>
-              <Typography component='h1' variant='h5'>
+              <Typography component="h1" variant="h5">
                 Log in to Camp4Everyone
               </Typography>
             </Grid>
@@ -126,50 +128,50 @@ export default function Login(props) {
               className={classes.margin}
               required
               fullWidth
-              label='Email'
-              variant='outlined'
-              id='email'
-              type='email'
-              name='email'
-              inputProps={{ style: { color: 'white' } }}
-              autoComplete='email'
+              label="Email"
+              variant="outlined"
+              id="email"
+              type="email"
+              name="email"
+              inputProps={{ style: { color: "white" } }}
+              autoComplete="email"
               value={values.email}
-              onChange={handleChange('email')}
+              onChange={handleChange("email")}
             />
             <CssTextField
               className={classes.margin}
               required
               fullWidth
-              label='Password'
-              variant='outlined'
-              id='password'
-              type='password'
-              name='password'
-              inputProps={{ style: { color: 'white' } }}
-              autoComplete='password'
+              label="Password"
+              variant="outlined"
+              id="password"
+              type="password"
+              name="password"
+              inputProps={{ style: { color: "white" } }}
+              autoComplete="password"
               value={values.password}
-              onChange={handleChange('password')}
+              onChange={handleChange("password")}
             />
           </Grid>
           <Grid item xs={12}>
             <Button
-              type='submit'
+              type="submit"
               fullWidth
-              variant='contained'
-              color='secondary'
-              style={{ marginTop: '0.8rem' }}
+              variant="contained"
+              color="secondary"
+              style={{ marginTop: "0.8rem" }}
             >
               Log in
             </Button>
           </Grid>
-          <Grid item xs={12} style={{ marginTop: '0.8rem' }}>
-            <Typography component='p'>
-              Already using Camp4Everyone?{' · '}
-              <Link to='/passwordRecovery'>Forgot password?</Link>
+          <Grid item xs={12} style={{ marginTop: "0.8rem" }}>
+            <Typography component="p">
+              Already using Camp4Everyone?{" · "}
+              <Link to="/passwordRecovery">Forgot password?</Link>
             </Typography>
-            <Typography component='p'>
-              New to Camp4Everyone?{' · '}
-              <Link to='/signup'>Sign up now »</Link>
+            <Typography component="p">
+              New to Camp4Everyone?{" · "}
+              <Link to="/signup">Sign up now »</Link>
             </Typography>
           </Grid>
         </form>
@@ -177,8 +179,8 @@ export default function Login(props) {
 
       <Snackbar
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
+          vertical: "bottom",
+          horizontal: "left"
         }}
         open={open}
         autoHideDuration={5000}
--- app/camp4everyone/src/js/services/firebase.js
+++ app/camp4everyone/src/js/services/firebase.js
@@ -1,8 +1,8 @@
-import firebase from 'firebase/app'
-import 'firebase/database'
-import 'firebase/storage'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/storage";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_APIKEY,
@@ -12,9 +12,10 @@ const firebaseApp = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID,
   measurementId: process.env.REACT_APP_MEASUREMENTID
-})
-const auth = firebase.auth()
-export const db = firebaseApp.firestore()
+});
+const auth = firebase.auth();
+export const db = firebaseApp.firestore();
+export const dbRef = firebaseApp.database();
 
 export const login = (email, password) => {
   return auth.signInWithEmailAndPassword(email, password);
@@ -26,54 +27,96 @@ export const signout = () => {
   return auth.signOut();
 };
 export const passwordRecovery = email => {
-  return auth.sendPasswordResetEmail(email)
-}
+  return auth.sendPasswordResetEmail(email);
+};
 
 export const addUser = (name, email) => {
   return db
-    .collection('users')
+    .collection("users")
     .add({
       name: JSON.stringify(name),
       email: JSON.stringify(email)
     })
     .then(function(docRef) {
-      console.log('Document written with ID: ', docRef.id)
+      console.log("Document written with ID: ", docRef.id);
     })
     .catch(function(error) {
-      console.error('Error adding document: ', error)
-    })
-}
+      console.error("Error adding document: ", error);
+    });
+};
 
 export const addPlace = (name, price) => {
   return db
-    .collection('places')
+    .collection("places")
     .add({
       name: JSON.stringify(name),
       price: price
     })
     .then(function(docRef) {
-      console.log('Document written with ID: ', docRef.id)
+      console.log("Document written with ID: ", docRef.id);
     })
     .catch(function(error) {
-      console.error('Error adding document: ', error)
-    })
-}
+      console.error("Error adding document: ", error);
+    });
+};
 export const addReservation = (user, place, price, date) => {
   return db
-    .collection('reservations')
+    .collection("reservations")
     .add({
       user: JSON.stringify(user),
       place: JSON.stringify(place),
       billing: price,
-      date: date,
+      date: date
     })
     .then(function(docRef) {
-      console.log('Document written with ID: ', docRef.id)
+      console.log("Document written with ID: ", docRef.id);
     })
     .catch(function(error) {
-      console.error('Error adding document: ', error)
-    })
-}
+      console.error("Error adding document: ", error);
+    });
+};
 export const getdb = () => {
   return db;
-}
+};
+
+/* export const updateUser = (user, namess) => {
+  console.log("Email del usuraio a Update: ", user.email);
+  console.log("Nombre del usuraio a Update: ", user.name);
+  console.log(maness);
+  user
+    .updateProfile({ name: "hello", email: "123@123.com" })
+    .then(function() {
+      console.log("User updated succesfully.");
+    })
+    .catch(function(error) {
+      console.error("Error updating user profile: ", error);
+    });
+  user
+    .updateEmail("123@123.com")
+    .then(function() {
+      console.log("User email updated succesfully.");
+    })
+    .catch(function(error) {
+      console.error("Error email user error: ", error);
+    });
+  console.log("Email del usuraio a Update: ", user.email);
+  console.log("Nombre del usuraio a Update: ", user.name);
+}; */
+
+export const updateUser = (userID, namess) => {
+  return dbRef
+    .ref("users/" + userID)
+    .set({
+      name: namess
+    })
+    .then(function() {
+      console.log("User updated succesfully.");
+    })
+    .catch(function(error) {
+      console.error("Error updating user profile: ", error);
+    });
+};
+
+export const getCurrentUser = () => {
+  return firebase.auth().currentUser;
+};
