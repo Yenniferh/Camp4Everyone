765181c4a9e0cfc9135d9f6f4baa71cd12909e48
DavidCuentasMar <david.cuentas@agswebconsulting.com>
2019-11-08 16:08:34 -0500

    git comment

--- app/camp4everyone/src/js/index.js
+++ app/camp4everyone/src/js/index.js
@@ -4,23 +4,8 @@ import Private from './private/index.js';
 import {Provider, Consumer} from '../AuthContext';
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import theme from "./theme";
-import { signout } from './services/firebase';
 
 function Main() {
-  /* const [isAuth, setIsAuth] = useState(false);
-  useEffect(() => {
-    const uid = sessionStorage.getItem("user");
-    uid !== null && setIsAuth(true);
-  },[isAuth]);
-
-  const setAuthentication = val => { 
-    if(!val){
-      signout();
-      sessionStorage.clear();
-    }  
-    setIsAuth(val);
-  } */
-
   return (
     <main>
       <MuiThemeProvider theme={theme}>
