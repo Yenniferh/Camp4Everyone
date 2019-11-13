f0c7e2ebfb14cb3e81af001558f27d1825514f06
yenniferh <yenniferh@uninorte.edu.co>
2019-11-13 16:23:35 -0500

    Page 404, responsive has not been implemented

--- app/camp4everyone/src/js/public/index.js
+++ app/camp4everyone/src/js/public/index.js
@@ -1,40 +1,34 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Landing from './Landing/'
-import Category from './Category/'
-import AboutUs from './AboutUs/'
-import Login from './Login/'
-import Signup from './Signup/'
-import PasswordRec from './PasswordRecovery'
-import CategoryNav from './CategoryNavigation'
-import Navbar from './Navbar/'
-import Footer from './Footer/'
-import TermsAndConditions from './TermsAndConditions/'
-import Profile from '../private/Profile/'
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Landing from "./Landing/";
+import Category from "./Category/";
+import AboutUs from "./AboutUs/";
+import Login from "./Login/";
+import Signup from "./Signup/";
+import PasswordRec from "./PasswordRecovery";
+import CategoryNav from "./CategoryNavigation";
+import Navbar from "./Navbar/";
+import Footer from "./Footer/";
+import TermsAndConditions from "./TermsAndConditions/";
+import NotFound from "../public/NotFound";
 
 function Public(props) {
   return (
     <main>
       <BrowserRouter>
         <Navbar></Navbar>
-        <Route path='/' exact component={Landing} />
-        <Route
-          path='/signup'
-          render={() => <Signup  />}
-        />
-        <Route
-          path='/login'
-          render={() => <Login  />}
-        />
-        <Route path='/category' component={Category} />
-        <Route path='/aboutus' component={AboutUs} />
-        <Route path='/termsandconditions' component={TermsAndConditions} />
-        <Route path='/passwordrecovery' component={PasswordRec} />
-        <Route path='/categorynav' component={CategoryNav} />
-        <Route path='/profile' component={Profile} />
+        <Route path="/" exact component={Landing} />
+        <Route path="/signup" render={() => <Signup />} />
+        <Route path="/login" render={() => <Login />} />
+        <Route path="/category" component={Category} />
+        <Route path="/aboutus" component={AboutUs} />
+        <Route path="/termsandconditions" component={TermsAndConditions} />
+        <Route path="/passwordrecovery" component={PasswordRec} />
+        <Route path="/categorynav" component={CategoryNav} />
+        <Route component={NotFound} />
         <Footer></Footer>
       </BrowserRouter>
     </main>
-  )
+  );
 }
-export default Public
+export default Public;
