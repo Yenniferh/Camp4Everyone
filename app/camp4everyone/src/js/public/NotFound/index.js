e55085427fe0dd9970db3c0c37fb83ad0e4a267b
Yennifer Herrera <yenniferh@uninorte.edu.co>
2019-11-18 12:39:22 -0500

    Route 404 fixed

--- app/camp4everyone/src/js/public/NotFound/index.js
+++ app/camp4everyone/src/js/public/NotFound/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import NotFoundLeft from "../../../images/not_found_60.svg";
-import NotFoundRight from "../../../images/page_not_found.svg";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import NotFoundLeft from '../../../images/not_found_60.svg';
+import NotFoundRight from '../../../images/page_not_found.svg';
 
 export default function NotFound() {
   return (
-    <Grid container className="not-found">
-      <Grid item sm={6} className="left-image">
-        <img src={NotFoundLeft} alt="Ups"></img>
+    <Grid container className='not-found'>
+      <Grid item md={6} className='left-image'>
+        <img src={NotFoundLeft} alt='Ups'></img>
       </Grid>
-      <Grid item sm={6} className="right-image">
-        <img src={NotFoundRight} alt="A problem has happen"></img>
-        <Typography variant="h4" component="p">
+      <Grid item sm={12} md={6} className='right-image'>
+        <img src={NotFoundRight} alt='A problem has happen'></img>
+        <Typography variant='h4' component='p'>
           Ups! what you are looking for has disappeared in the forest.
         </Typography>
-        <Typography variant="body2" component="p" className="right-paragraph">
-          But keep calm, you can view other places. Try to check our{" "}
-          <Link to="/category">category section</Link>.
+        <Typography variant='body2' component='p' className='right-paragraph'>
+          But keep calm, you can view other places. Try to check our{' '}
+          <Link to='/category'>category section</Link>.
         </Typography>
       </Grid>
     </Grid>
--- app/camp4everyone/src/js/public/index.js
+++ app/camp4everyone/src/js/public/index.js
@@ -1,31 +1,33 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Landing from "./Landing/";
-import Category from "./Category/";
-import AboutUs from "./AboutUs/";
-import Login from "./Login/";
-import Signup from "./Signup/";
-import PasswordRec from "./PasswordRecovery";
-import CategoryNav from "./CategoryNavigation";
-import Navbar from "./Navbar/";
-import Footer from "./Footer/";
-import TermsAndConditions from "./TermsAndConditions/";
-import NotFound from "../public/NotFound";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Landing/';
+import Category from './Category/';
+import AboutUs from './AboutUs/';
+import Login from './Login/';
+import Signup from './Signup/';
+import PasswordRec from './PasswordRecovery';
+import CategoryNav from './CategoryNavigation';
+import Navbar from './Navbar/';
+import Footer from './Footer/';
+import TermsAndConditions from './TermsAndConditions/';
+import NotFound from '../public/NotFound';
 
-function Public(props) {
+function Public() {
   return (
     <main>
       <BrowserRouter>
         <Navbar></Navbar>
-        <Route path="/" exact component={Landing} />
-        <Route path="/signup" render={() => <Signup />} />
-        <Route path="/login" render={() => <Login />} />
-        <Route path="/category" component={Category} />
-        <Route path="/aboutus" component={AboutUs} />
-        <Route path="/termsandconditions" component={TermsAndConditions} />
-        <Route path="/passwordrecovery" component={PasswordRec} />
-        <Route path="/categorynav" component={CategoryNav} />
-        <Route component={NotFound} />
+        <Switch>
+          <Route path='/' exact component={Landing} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/login' component={Login} />
+          <Route path='/category' component={Category} />
+          <Route path='/aboutus' component={AboutUs} />
+          <Route path='/termsandconditions' component={TermsAndConditions} />
+          <Route path='/passwordrecovery' component={PasswordRec} />
+          <Route path='/categorynav' component={CategoryNav} />
+          <Route component={NotFound} />
+        </Switch>
         <Footer></Footer>
       </BrowserRouter>
     </main>
